71cba513567d0f8034e4ecf3b49c99ba2d8ca21e
Jia <756951628@qq.com>
2019-11-13 09:31:27 +0800

    静态雏形

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # particle-line 点线图
-canvas练习，制作点线图
+canvas+typescript练习，制作点线图
 
-## 需求
+## 目标
 1. ~~新建项目~~
    - ~~typescript~~
    - ~~webpack~~
    - ~~eslint~~
 2. 制作基础点线图
+   - canvas能自适应
+   - 添加canvas节点位置可选择
+   - 在canvas中添加随机点并使其移动
+   - 在一定距离内的点之间应有连线
+   - 超出屏幕的点应清除
 3. 加入点随鼠标移动的效果
+
--- src/particle-line.ts
+++ src/particle-line.ts
@@ -1,19 +1,87 @@
+const color = '#3B4250'
+const background = '#FFFFFF'
+
+interface Particle {
+  x: number;
+  y: number;
+  r: number;
+  color: string;
+  vx: number;
+  vy: number;
+}
+
 class ParticleLine {
-  constructor(node: Node | null) {
+  width = 300
+  height = 300
+  ctx: CanvasRenderingContext2D
+  particleNum = 50
+  particleArr: Particle[] = []
+
+  constructor(node: Node | null = null) {
     const body = document.querySelector('body') as HTMLBodyElement
-    const width = window.innerWidth
-    const height = window.innerHeight
+    this.width = window.innerWidth
+    this.height = window.innerHeight
 
     const canvas = document.createElement('canvas')
-    canvas.style.width = width + 'px'
-    canvas.style.height = height + 'px'
+    canvas.innerText = '您的浏览器不支持 Canvas!'
+    canvas.width = this.width
+    canvas.height = this.height
 
-    this.insertBefore()
+    if (node === null) node = body.childNodes[0]
+    body.insertBefore(canvas, node)
+    this.ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+
+    this.draw()
   }
 
-  insertBefore (node: node | null) {
-    return this.body.insertBefore(canvas, node)
+  draw (): void {
+    const ctx = this.ctx
+    ctx.fillStyle = background
+    ctx.fillRect(0, 0, this.width, this.height)
+
+    this.particleNum = this.randomRange(90, 140)
+
+    for (let i = 0; i < this.particleNum; i++) {
+      if (this.particleArr.length < i + 1) {
+        const _r = 2.5
+
+        const _p = {
+          x: this.randomRange(_r, this.width - _r),
+          y: this.randomRange(_r, this.height - _r),
+          r: _r,
+          color: color,
+          vx: this.randomRange(-2, 2),
+          vy: this.randomRange(-2, 2)
+        }
+        this.particleArr.push(_p)
+      }
+      ctx.beginPath()
+      ctx.arc(this.particleArr[i].x, this.particleArr[i].y, this.particleArr[i].r, 0, Math.PI * 2)
+      ctx.fillStyle = this.particleArr[i].color
+      ctx.fill()
+    }
+
+    // 上方画点过程中会新增点，无法遍历所有点进行线段的描绘
+    for (let i1 = 0; i1 < this.particleNum; i1++) {
+      const _p1 = this.particleArr[i1]
+      for (let i2 = i1 + 1; i2 < this.particleNum; i2++) {
+        const _p2 = this.particleArr[i2]
+        const _l = Math.sqrt(Math.pow(_p1.x - _p2.x, 2) + Math.pow(_p1.y - _p2.y, 2))
+        if (_l < 100) {
+          ctx.beginPath()
+          ctx.moveTo(_p1.x, _p1.y)
+          ctx.lineTo(_p2.x, _p2.y)
+          ctx.strokeStyle = color
+          ctx.stroke()
+        }
+      }
+    }
   }
+
+  randomRange (min: number, max: number, ): number {
+    return Math.floor(min + Math.random() * (max - min + 1))
+  }
+
 }
 
 export default ParticleLine
